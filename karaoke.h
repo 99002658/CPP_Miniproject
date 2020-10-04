6a6428f4b3c2b44543c73815dd58e342b1fc94e0
99002658 <71540823+99002658@users.noreply.github.com>
2020-10-05 02:54:49 +0530

    Add files via upload

---
+++ karaoke.h
@@ -0,0 +1,19 @@
+#ifndef _KARAOKE_H_
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <iostream>
+
+#define _KARAOKE_H_
+class KaraokeRest{
+
+    public:
+    KaraokeRest();
+    KaraokeRest(int);
+};
+
+#endif
